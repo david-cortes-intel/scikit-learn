4fab63dfaa34253e3df6fc44c291b17765af765f
David Cortes <david.cortes@intel.com>
2025-12-10 12:57:28 +0100

    try manual reimport

--- sklearn/linear_model/tests/test_logistic.py
+++ sklearn/linear_model/tests/test_logistic.py
@@ -654,6 +654,7 @@ def test_multinomial_logistic_regression_string_inputs():
 
 @pytest.mark.parametrize("csr_container", CSR_CONTAINERS)
 def test_logistic_cv_sparse(global_random_seed, csr_container):
+    from sklearn.linear_model._logistic import LogisticRegressionCV
     X, y = make_classification(
         n_samples=100, n_features=5, random_state=global_random_seed
     )
